a906c853f76b11242280a9b0cfd509ed1004adef
USER-PC\Professional <rrossavich@gmail.com>
2022-03-27 18:19:37 +0300

    Start with Global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    body {
+      font-family: 'Open Sans', sans-serif;
+    }
+    h1, h2, h3 {
+      font-family: 'Righteous', cursive;
+    }
+`
+
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <h1>Other Text</h1>
+      <GlobalStyle/>
+      <div> Hello Fuck world </div>
+    </>
   );
 }
 
