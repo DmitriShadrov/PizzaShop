80fd058daa861148b5e2317bf439b233b7005fb8
USER-PC\Professional <rrossavich@gmail.com>
2022-04-16 17:47:53 +0300

    Add Order Container

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -19,13 +19,30 @@ const OrderContent = styled(DialogContent)`
     padding: 20px;
     height: 100%;
 `;
-
+const OrderContainer = styled.div`
+    padding: 10px 0px;
+    border-bottom: 1px solid grey;
+`;
+const OrderItem = styled.div`
+    padding: 10x 0px;
+`;
 
 export function Order({orders}){
     return  <OrderStyled>                
-                {orders.length === 0 ? <OrderContent>Your order's looking pretty empty</OrderContent> : 
-                    <OrderContent>Found {orders.length} orders</OrderContent>
-                }  
+                {orders.length === 0 ? (<OrderContent>Your order's looking pretty empty</OrderContent>
+                ) : (
+                    <OrderContent> 
+                        {" "}
+                        <OrderContainer> Your order: </OrderContainer> {" "}
+                        {orders.map(order => (
+                            <OrderContainer>
+                                <OrderItem>
+                                    {order.name}
+                                </OrderItem>
+                            </OrderContainer>
+                        ))}
+                    </OrderContent>
+                )}  
                 <DialogFooter>
                     <ConfirmButton>Checkout</ConfirmButton>
                 </DialogFooter>                
